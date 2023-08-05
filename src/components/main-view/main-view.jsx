63816e4268507c53f98f389fba744115017b8020
Skogab <114601288+Skogab@users.noreply.github.com>
2023-08-05 15:28:04 +0200

    Update Movie-Filter

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
@@ -8,6 +8,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
 import { ProfileView } from "../profile-view/profile-view";
 import { UpdateForm } from "../profile-view/update-form";
+import { MovieFilter } from "../movie-filter/movie-filter"; // Neue Filmfilter-Komponente
 
 import "./main-view.scss";
 
@@ -52,86 +53,116 @@ export const MainView = () => {
 		localStorage.clear();
 	};
 
+	// State für die ausgewählten Filteroptionen
+	const [selectedGenre, setSelectedGenre] = useState("");
+	const [selectedDirector, setSelectedDirector] = useState("");
+
+	// Funktion zum Filtern der Filme basierend auf den ausgewählten Optionen
+	const filterMovies = () => {
+		let filteredMovies = movies;
+
+		if (selectedGenre) {
+			filteredMovies = filteredMovies.filter((movie) => movie.genre === selectedGenre);
+		}
+
+		if (selectedDirector) {
+			filteredMovies = filteredMovies.filter((movie) => movie.director === selectedDirector);
+		}
+
+		return filteredMovies;
+	};
+
+	// Filme nach den ausgewählten Filteroptionen filtern
+	const filteredMovies = filterMovies();
+
 	return (
 		<BrowserRouter>
 			<NavigationBar user={user} onLoggedOut={handleLogout} />
-			<Row className="justify-content-md-center">
-				<Routes>
-					<Route
-						path="/signup"
-						element={
-							<>
-								{user ? (
-									<Navigate to="/" />
-								) : (
-									<Col md={5}>
-										<SignupView />
-									</Col>
-								)}
-							</>
-						}
-					/>
-					<Route
-						path="/login"
-						element={
-							<>
-								{user ? (
-									<Navigate to="/" />
-								) : (
-									<Col md={5}>
-										<LoginView onLoggedIn={(user) => setUser(user)} />
-									</Col>
-								)}
-							</>
-						}
-					/>
-					<Route
-						path="/movies/:movieId"
-						element={
-							<>
-								{!user ? (
-									<Navigate to="/login" replace />
-								) : movies.length === 0 ? (
-									<Col>The list is empty!</Col>
-								) : (
-									<Col md={8}>
-										<MovieView movies={movies} />
-									</Col>
-								)}
-							</>
-						}
-					/>
-					<Route
-						path="/"
-						element={
-							<>
-								{!user ? (
-									<Navigate to="/login" replace />
-								) : movies.length === 0 ? (
-									<Col>The list is empty!</Col>
-								) : (
-									<>
-										{movies.map((movie) => (
-											<Col className="mb-4" key={movie.id} md={4}>
-												<MovieCard movie={movie} />
-											</Col>
-										))}
-									</>
-								)}
-							</>
-						}
-					/>
-
-					<Route
-						path="/profile"
-						element={<>{user ? <ProfileView user={user} movies={movies} /> : <Navigate to="/login" replace />}</>}
-					/>
-					<Route
-						path="/profile/update"
-						element={<>{user ? <UpdateForm user={user} /> : <Navigate to="/login" replace />}</>}
-					/>
-				</Routes>
-			</Row>
+			<Container>
+				{/* Filmfilter-Komponente */}
+				<MovieFilter
+					selectedGenre={selectedGenre}
+					selectedDirector={selectedDirector}
+					setSelectedGenre={setSelectedGenre}
+					setSelectedDirector={setSelectedDirector}
+				/>
+				<Row className="justify-content-md-center">
+					<Routes>
+						<Route
+							path="/signup"
+							element={
+								<>
+									{user ? (
+										<Navigate to="/" />
+									) : (
+										<Col md={5}>
+											<SignupView />
+										</Col>
+									)}
+								</>
+							}
+						/>
+						<Route
+							path="/login"
+							element={
+								<>
+									{user ? (
+										<Navigate to="/" />
+									) : (
+										<Col md={5}>
+											<LoginView onLoggedIn={(user) => setUser(user)} />
+										</Col>
+									)}
+								</>
+							}
+						/>
+						<Route
+							path="/movies/:movieId"
+							element={
+								<>
+									{!user ? (
+										<Navigate to="/login" replace />
+									) : movies.length === 0 ? (
+										<Col>The list is empty!</Col>
+									) : (
+										<Col md={8}>
+											<MovieView movies={movies} />
+										</Col>
+									)}
+								</>
+							}
+						/>
+						<Route
+							path="/"
+							element={
+								<>
+									{!user ? (
+										<Navigate to="/login" replace />
+									) : filteredMovies.length === 0 ? (
+										<Col>The list is empty!</Col>
+									) : (
+										<>
+											{filteredMovies.map((movie) => (
+												<Col className="mb-4" key={movie.id} md={4}>
+													<MovieCard movie={movie} />
+												</Col>
+											))}
+										</>
+									)}
+								</>
+							}
+						/>
+						<Route
+							path="/profile"
+							element={<>{user ? <ProfileView user={user} movies={movies} /> : <Navigate to="/login" replace />}</>}
+						/>
+						<Route
+							path="/profile/update"
+							element={<>{user ? <UpdateForm user={user} /> : <Navigate to="/login" replace />}</>}
+						/>
+					</Routes>
+				</Row>
+			</Container>
 		</BrowserRouter>
 	);
 };
